862c3d66367c20b1614f8725d5089777d66ad966
gammastudent at wygal-builders <nelsonderecha@gmail.com>
2022-12-12 17:17:36 -0800

    Hash generator: json, two flat files

--- hash-generate-from-all-pep-json.js
+++ hash-generate-from-all-pep-json.js
@@ -8,18 +8,17 @@ const { writeFile } = require('fs');
 const { readFile } = require('fs').promises;
 const crypto = require('crypto');
 
+let fullPath = "C:/Users/gammastudent/Documents/2022-FALL/javascript/change-source/"
+
 let dirName = './DATA/'
 let fileName = './Homo_sapiens.GRCh38.pep.all.json';
-fileName = './out671.txt';
-fileName = './tmp.txt';
-fileName = '../../homo-sapiens-gr38/Homo_sapiens.GRCh38.pep.all.txt'; // JSON.stringigy(fastaOjbArray) invalid string length
-fileName = 'gbpri1.fsa_aa.out.json';
-fileName = './Homo_sapiens.GRCh38.pep.all.label-seq.out.json'
-fileName = 'gbpri1.fsa_aa.label-seq.out.json'; //'.label-seq.out.json'
+fileName = 'gbpri1.fsa_aa.label-seq.out.json'; //'.label-seq.out.json' 
+
+fullPath += dirName + fileName;
 
 const readContentFile = async fileName => {
   try {
-    const mypromise = await readFile(fileName, { encoding: 'utf8' });
+    const mypromise = await readFile(fullPath, { encoding: 'utf8' });
     // return mypromise.then(i => console.log(i)); // mypromise.then is not a function
     return mypromise;
 
@@ -29,21 +28,9 @@ const readContentFile = async fileName => {
   }
 };
 /**object: ensemLabel:"", synonymArray: [], seqHash: "" seqArray: "", seqBelongArray */
-let pepObj ={};
-let pepObjArray = [];
-const pepAllDict = {}; /**key/value   ensemLabelHash/seqHash */
-/**
- *            item.hash = null; // createHash().update().digest()
-            let hashMethod = 'sha256';
-            hashMethod = 'sha1';
-            hashMethod = 'md5';
-            item.hash = crypto.createHash(hashMethod).update(path.basename(i)).digest('hex');
-            // console.log(item); // *****
-            result.push(item);
- 
- */
 
-readContentFile(fileName).then( jsonObjString => { // it is read as a  string
+
+readContentFile(fullPath).then( jsonObjString => { // it is read as a  string
   let myModifier = "NelsonLopez";
   let buildFlatHashFile = "";
   let buildFlatHashToLabel = "";
@@ -63,7 +50,7 @@ readContentFile(fileName).then( jsonObjString => { // it is read as a  string
     let seqLenght;
     let modifiedLabelHash = myModifier + jsonObj[i].label; //not used look below....
     labelHash = crypto.createHash(hashMethod).update(myModifier + jsonObj[i].label).digest('hex');
-    seqHash = crypto.createHash(hashMethod).update(jsonObj[i].seq).digest('hex');
+    seqHash = crypto.createHash(hashMethod).update(myModifier + jsonObj[i].seq).digest('hex');
     seqLenght = jsonObj[i].seq.length;
     // console.log(`labelHash, seqHash : ${labelHash} ${seqHash}`); // *****consolelog
     seqObjHash.labelHash = labelHash;
@@ -71,48 +58,47 @@ readContentFile(fileName).then( jsonObjString => { // it is read as a  string
     seqObjHash.seqLenght = seqLenght;
     seqObjHashArray.push(seqObjHash);
 
-    // seqHashToLabelObj.label = jsonObj[i].label;
-    // seqHashToLabelObj.seqHash = seqHash;
-    // seqHashToLabelObj.seqLenght = seqLenght;
-    // seqHashToLabelArray.push(seqHashToLabelObj);
-
     buildFlatHashToLabel += `${seqHash}\t${jsonObj[i].label}\t${seqLenght}\n`
 
     buildFlatHashFile += `${seqHash}\t${labelHash}\t${seqLenght}\n`
   }
-  // console.log(typeof pepAllDict);
-  // console.log(typeof jsonObj);
-  // console.log(typeof seqObjHashArray.length);
-  console.log(seqObjHashArray.length + " line77");
-  console.log(seqHashToLabelArray.length + ' line78 seqHashToLabelArray.length')
+
+  console.log(seqObjHashArray.length + " line90");
+  console.log(seqHashToLabelArray.length + ' line91 seqHashToLabelArray.length')
   // for (let i = seqObjHashArray.length - numOfRecords; i < numOfRecords; i++){
   //   console.log(jsonObj[i])
   //   console.log(seqObjHashArray[i]);
   // }
   // return seqObjHashArray; // no effect whatsoever
-  for (item of seqHashToLabelArray) {
-    if (item.seqHash === "55a5e2d656c64d17ab5dd5a0a7c902875d3810ee"){
-      console.log(item.label);
-    }
-  }
 
-              // Homo_sapiens.GRCh38.pep.all.label-seq.out
-              // gbpri1.fsa_aa.label-seq.
-    //'.label-seq.out.json'
-  // writeFile('gbpri1.fsa_aa.out.sha1.json', JSON.stringify(seqObjHashArray), (error) => {
-
-  writeFile(dirName + fileName + '.hashed.json', JSON.stringify(seqObjHashArray), (error) => {
+  writeFile(fullPath + '.hashed.json', JSON.stringify(seqObjHashArray), (error) => {
     if (error) console.log(error);
     else console.log('success sha1 json')
   })
-  writeFile(dirName + fileName + '.hash2label.flat', buildFlatHashToLabel, error => {
+  writeFile(fullPath + '.hash2label.flat', buildFlatHashToLabel, error => {
     if (error) console.log(error + 'line88');
     else console.log('writen sequence hash to label FLAT')
   })
-  writeFile(dirName + fileName + '-seqHash-labelHash-seqLength.flat', buildFlatHashFile, error => {
+  writeFile(fullPath + '-seqHash-labelHash-seqLength.flat', buildFlatHashFile, error => {
     if (error) console.log(error + ' line102');
-    else console.log('flat file written!!');
+    else console.log('flat file writfullPath');
   })
-  
+
+
 });
-  
+/**
+ * 12.12.2022
+ * ALGORiTHM
+ * 1) input a json with label/fasta aa pairs
+ * 2) read with fs.promises
+ * 3) dot then( process the string);
+ * 4) PROCESS: 
+ * 5) JSON.parse the string
+ * 6) for loop each line
+ * 7) Generate hashes of label, fasta_aa, seq length
+ * 8) add to array of json, and two flat files: one with seqHash and full label
+ * 9) second flat file with seqHash, labelHash, and length. 
+ * 10) add other properties at a later time
+ * 11) write to files and quit.
+ * 
+ */
